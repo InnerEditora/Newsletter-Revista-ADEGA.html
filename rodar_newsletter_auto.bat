9f01ad220fd27a3e50913ce80e1cb328ce78fcc5
InnerEditora <felipe.granata@innereditora.com.br>
2026-06-05 16:59:23 -0300

    05/06
    nova news

--- rodar_newsletter_auto.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ================================
-echo  Newsletter Automatica - ADEGA
-echo ================================
-cd /d C:\Users\felip\Desktop\Newsletter
-python newsletter_adega_auto.py
-echo.
-echo Processo finalizado.
-pause
